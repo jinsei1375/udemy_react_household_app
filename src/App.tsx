0c998ecd5d11ff52db1a9feb165041ce50ac759d
murakami <murakami@kokochie.jp>
2024-03-31 22:38:48 +0900

    サイドバー調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
 import './App.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Report from './pages/Report';
 import Nomatch from './pages/Nomatch';
 import AppLayout from './components/layout/AppLayout';
+import {theme} from './theme/theme'
+import { ThemeProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
 
 function App() {
   return (
+    <ThemeProvider theme={theme}>  
+      <CssBaseline />
     <Router>
       <Routes>
         <Route path='/' element={<AppLayout />} >
@@ -17,6 +21,7 @@ function App() {
         </Route>
       </Routes>
     </Router>
+    </ThemeProvider>
   );
 }
 
