b8e40e05f4463c8dd423fc8e5970a8a614722ab6
murakami <murakami@kokochie.jp>
2024-04-02 09:44:25 +0900

    firesotreのセットアップ、データ取得

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,36 @@ import AppLayout from './components/layout/AppLayout';
 import {theme} from './theme/theme'
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { Transaction } from './types';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from './firebase';
 
 function App() {
+
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fetchTransactions = async() => {
+      try {
+        const querySnapShot = await getDocs(collection(db, "Transactions"));
+        // console.log(querySnapShot);
+        const transactionsData = querySnapShot.docs.map((doc) => {
+          // console.log(doc.id, doc.data());
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction
+        });
+        console.log(transactionsData);
+        setTransactions(transactionsData);
+      } catch(err) {
+        
+      }
+    }
+    fetchTransactions();
+  }, [])
+
   return (
     <ThemeProvider theme={theme}>  
       <CssBaseline />
---
+++ src/types/index.ts
@@ -0,0 +1,12 @@
+export type TransactionType = "income" | "expense";
+export type IncomeCategory = "給与" | "副収入" | "お小遣い";
+export type ExpenseCategory = "食費" | "日用品" | "交際費" | "娯楽" | "固定費";
+
+export interface Transaction {
+  id: string,
+  date: string,
+  amount: number,
+  content: string,
+  type: TransactionType,
+  category: IncomeCategory | ExpenseCategory,
+}
