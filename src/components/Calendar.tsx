09d916d360a4da339abf3106583f63a6b520725e
murakami <murakami@kokochie.jp>
2024-04-04 09:38:59 +0900

    カレンダー表示

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import jaLocale from "@fullcalendar/core/locales/ja"
+import { EventContentArg } from '@fullcalendar/core'
+import "../calendar.css"
 
 const Calendar = () => {
+
+  const events = [
+    {title: 'Meeting', start: "2024-04-04"},
+    {title: 'Meeting', start: "2024-04-04", income: 300, expense: 400, balance: -100},
+  ]
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div className='money' id='event-income'>
+          {eventInfo.event.extendedProps.income}
+        </div>
+        <div className='money' id='event-expense'>
+          {eventInfo.event.extendedProps.expense}
+        </div>
+        <div className='money' id='event-balance'>
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    )
+  }
+
   return (
-    <div>Calendar</div>
+    <FullCalendar 
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView='dayGridMonth'
+      events={events}
+      eventContent={renderEventContent}
+    />
   )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,18 @@ import Calendar from '../components/Calendar'
 import TransactionMenu from '../components/TransactionMenu'
 import TransactionForm from '../components/TransactionForm'
 import { Transaction } from '../types'
+import { useState } from 'react'
+import { format } from 'date-fns'
 
 interface HomeProps {
   monthlyTransactions: Transaction[],
 }
 
 const Home = ({monthlyTransactions}: HomeProps) => {
+  const today = format (new Date(), "yyyy-MM-dd");
+  console.log(today);
+  const [currentDay, setCurrentDay] = useState(today);
+
   return (
     <Box sx={{ display: "flex" }}>
       {/* 左側コンテンツ */}
